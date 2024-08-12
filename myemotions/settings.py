bbd35297fbc1db29902333016f6c246a0961a383
Eymer <ezequielvanegas248@gmail.com>
2024-08-12 12:07:58 -0600

    build.sh test

--- myemotions/settings.py
+++ myemotions/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'myemotions.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default= 'postgresql://postgres:postgres@localhost/postgres',
+        default= 'psql://postgres:postgres@localhost/postgres',
         conn_max_age=600
     )
 }
